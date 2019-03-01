e7de6146dfbd47e1f3e161b4a0849000cf2230ca
Andrade <andradv@ad.esi.adp.com>
2019-03-01 15:55:40 -0300

    Isolando o config do express e usando o Consign.

--- payfast/index.js
+++ payfast/index.js
@@ -1,12 +1,7 @@
-var express = require('express');
-var app = express();
+// A FUNÇÃO LISTEN DIZ EM QUAL PORTA IRÁ RODAR A APLICAÇÃO.
+var app = require('./config/custom-express') ();
 
-// A FUNÇÃO LISTEN DIZ EM QUAL IRÁ RODAR A APLICAÇÃO.
 app.listen(3000, function() {
     console.log('Servidor rodando na porta 3000.');
 });
 
-app.get('/teste',function (req,res) {
-    console.log('Recebida requisição de teste na porta 3000.');
-    res.send('OK');
- });
