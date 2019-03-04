b7995080cb29610873afab443991f82e493a17fe
Andrade <andradv@ad.esi.adp.com>
2019-03-04 11:36:12 -0300

    Setando o status e a data de pagamento.

--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -3,5 +3,15 @@ module.exports = function(app) {
         console.log('Recebida requisição de teste na porta 3000.');
         res.send('OK');
     });
-}
 
+
+app.post('/pagamentos/pagamento', function(req,res) {
+    var pagamento = req.body;
+    console.log('Processando uma requisição de um novo pagamento.');
+
+    pagamento.status = ' CRIADO ';
+    pagamento.data = new Date;
+
+    res.send(pagamento);
+    });
+}
